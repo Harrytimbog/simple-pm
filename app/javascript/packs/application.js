d13af53526b6a948335499d8a6efa7c4a867edfa
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-04-19 22:21:01 +0100

    Add drag and drop feature

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import Sortable from 'sortablejs';
 
 import "../stylesheets/application"
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +16,11 @@ import "../stylesheets/application"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "controllers"
+
+
+document.addEventListener('turbolinks:load', () => {
+  var el = document.getElementById('tasks-list');
+  var sortable = Sortable.create(el, { animation: 150 });
+})
